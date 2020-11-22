d8057409939cd033dc7d27fa57aa989c19dbcfcf
FlyingPolarBear <47551143+FlyingPolarBear@users.noreply.github.com>
2020-11-22 20:43:28 +0800

    Add Viterbi Algorithm and some comments

--- HMM.py
+++ HMM.py
@@ -11,20 +11,20 @@ class HMM():
     def __init__(self, N, K, O):
         self.T = O.shape[0]  # 序列长度
         self.N = N  # 状态集合取值数
-        self.K = K
-        self.O = O
-        self.A = np.ones((N, N))/N
-        self.B = np.ones((N, K))/K
-        self.pi = np.ones(N)/N
+        self.K = K  # 观测集合取值数
+        self.O = O  # 观测值O: Tx1
+        self.A = np.ones((N, N))/N  # A: NxN
+        self.B = np.ones((N, K))/K  # B: NxK
+        self.pi = np.ones(N)/N  # pi: Nx1
 
-    def ModelInit(self, A, B, pi, O):
+    def ModelInit(self, A, B, pi, O):  # 适用于概率计算和预测问题的初始化
         self.T = O.shape[0]  # 序列长度
         self.N = B.shape[0]  # 状态集合取值数
-        self.K = B.shape[1]
-        self.A = A
-        self.B = B
-        self.pi = pi
-        self.O = O
+        self.K = B.shape[1]  # 观测集合取值数
+        self.O = O  # 观测值O: Tx1
+        self.A = A  # A: NxN
+        self.B = B  # B: NxK
+        self.pi = pi  # pi: Nx1
 
     def Alpha(self):  # alpha: TxN
         alpha = np.zeros([self.T, self.N])
@@ -91,14 +91,32 @@ class HMM():
         self.I = np.argmax(self.gamma, axis=1)
         print("Approximate I =", self.I)
 
+    def Viterbi(self, A, B, pi, O):
+        self.ModelInit(A, B, pi, O)
+        delta = np.zeros((self.T, self.N))
+        psi = np.zeros((self.T, self.N))
+        delta[0] = self.pi*self.B[:, O[0]]  # 初始化
+        for t in range(1, self.T):  # 递推
+            for i in range(self.N):
+                delta[t][i] = np.max(delta[t-1]*A[:, i])*B[i, O[t]]
+                psi[t][i] = np.argmax(delta[t-1]*A[:, i])
+        self.P = np.max(delta[self.T-1])
+        self.I = np.zeros(self.T, dtype=np.int)
+        self.I[self.T-1] = np.argmax(delta[self.T-1])  # 终止
+        for t in range(self.T-2, -1, -1):  # 最优路径回溯
+            self.I[t] = psi[t+1][self.I[t+1]]
+        print("Viterbi I =", self.I)
 
+
+# 《统计学习方法》中的示例
 A = np.array([[0.5, 0.2, 0.3], [0.3, 0.5, 0.2], [0.2, 0.3, 0.5]])  # A: NxN
 B = np.array([[0.5, 0.5], [0.4, 0.6], [0.7, 0.3]])  # B: NxK
 pi = np.array([0.2, 0.4, 0.4])  # pi: Nx1
 O = np.array([0, 1, 0])  # O: Tx1
 
 m = HMM(B.shape[0], B.shape[1], O)
-m.Forward(A, B, pi, O)
-m.Backward(A, B, pi, O)
-m.Baum_Welch(10)
-m.Approximate(A, B, pi, O)
+m.Forward(A, B, pi, O)  # 概率计算：前向算法
+m.Backward(A, B, pi, O)  # 概率计算：后向算法
+m.Approximate(A, B, pi, O)  # 预测问题：近似算法
+m.Viterbi(A, B, pi, O)  # 预测问题：维特比算法
+m.Baum_Welch(10)  # 学习问题：Baum-Welch算法
